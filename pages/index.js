3692d652a8afe3a5bf757cf97f5f947536218ec4
vagabadu <shinno.ma30@gmail.com>
2023-04-04 04:39:26 +0300

    profile pic added and styled

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
-import { Container, Box, Heading } from "@chakra-ui/react";
+import {
+    Container,
+    Box,
+    Heading,
+    Image,
+    useColorModeValue,
+} from "@chakra-ui/react";
 
 const Page = () => {
     return (
         <Container>
-            <Box borderRadius="lg" p={3} mb={6} align="center">
+            <Box
+                borderRadius="lg"
+                bg={useColorModeValue("whiteAlpha.500", "whiteAlpha.200")}
+                p={2}
+                mb={6}
+                mt={6}
+                align="center"
+            >
                 Hello World! I&apos;m Shinno, a Syrian Japanese H4CKER based in
                 Lebanon.
             </Box>
@@ -13,6 +26,23 @@ const Page = () => {
                         Shinno Taguchi
                     </Heading>
                     @Vagabadu ( Arabophile / Developer / H4cker )
+                </Box>
+                <Box
+                    flexShrink={0}
+                    mt={{ base: 4, md: 0 }}
+                    ml={{ md: 6 }}
+                    align="center"
+                >
+                    <Image
+                        borderColor="whiteAlpha.800"
+                        borderWidth={2}
+                        borderStyle="solid"
+                        maxWidth="100px"
+                        display="inline-block"
+                        borderRadius="full"
+                        src="/images/shinno.png"
+                        alt="Profile Image"
+                    />
                 </Box>
             </Box>
         </Container>
