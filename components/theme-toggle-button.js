12d4dcc52672fe4c8c18cecad3a44f4f7f48cc67
vagabadu <shinno.ma30@gmail.com>
2023-04-06 13:08:37 +0300

    toggle button animation using framer motion complete

--- components/theme-toggle-button.js
+++ components/theme-toggle-button.js
@@ -1,16 +1,26 @@
 import { useColorMode, useColorModeValue, IconButton } from "@chakra-ui/react";
+import { AnimatePresence, motion } from "framer-motion";
 import { SunIcon, MoonIcon } from "@chakra-ui/icons";
 
 const ThemeToggleButton = () => {
     const { toggleColorMode } = useColorMode();
 
     return (
-        <IconButton
-            aria-label="Toggle theme"
-            colorScheme={useColorModeValue("purple", "orange")}
-            icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
-            onClick={toggleColorMode}
-        ></IconButton>
+        <AnimatePresence mode="wait" initial={false}>
+            <motion.div
+                style={{ display: "inline-block" }}
+                key={useColorModeValue("light", "dark")}
+                animate={{ rotate: 270 }}
+                transition={{ duration: 0.5 }}
+            >
+                <IconButton
+                    aria-label="Toggle theme"
+                    colorScheme={useColorModeValue("purple", "orange")}
+                    icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
+                    onClick={toggleColorMode}
+                ></IconButton>
+            </motion.div>
+        </AnimatePresence>
     );
 };
 
